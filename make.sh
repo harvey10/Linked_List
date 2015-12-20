d49b83785f7fdc749127839b1cff24a4df77312d
Jamie Harvey <harvey.jamie.j@gmail.com>
2015-12-20 14:49:30 -0800

    Fixed script

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ echo -e "Please enter file name"
 read file
 if [ -e $file.cpp ]; then
     clear
-    g++ -Wall -Wextra $file.cpp -o $file.o 2> error.txt
+    g++ -Wall -Wextra main.cpp $file.cpp  -o $file.o 2> error.txt
     if [ -e $file.o ]; then
 	./$file.o
     fi
